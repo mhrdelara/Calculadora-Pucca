70a0b29eb6275677d09c8ffb70928e5d75d6c450
mhrdelara <mariaheloisarochadelara015@gmail.com>
2025-04-25 16:45:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function calculadora(){
-    let number = calculadora.getElementById('number')
-
-
-
-
-
-
+function adicionarValor(valor){
+    let input = document.getElementById("text")
+    input.value += valor
 }
+
+function apagarValor(){
+    let input = document.getElementById("text")
+    input.value = ""
+}
+
+function aparecerValor(){
+    let input = document.getElementById("text")
+    input.value = eval(input.value)
+}
+
+eval.document.getElementById("text").value
+
